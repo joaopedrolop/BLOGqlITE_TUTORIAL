d1ce7b9a11035dc48f1dd00363a6f517b8a7c560
joaopedrolop <joao.p.freire6@aluno.senai.br>
2025-02-21 15:14:38 -0300

    PRIMEIRO COMMIT

---
+++ Exercicio/ex1_2.js
@@ -0,0 +1,8 @@
+// Para usar o prompt no nodejs é preciso installar essa lib 'prompt-sync'
+
+let prompt = require("prompt-sync");
+prompt = prompt();
+
+const nl=prompt("digite o primeiro número");
+console.log(n1);
+
