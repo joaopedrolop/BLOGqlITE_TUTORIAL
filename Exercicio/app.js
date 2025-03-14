f79ab8d250e3c9298d3d9e78f4c33f1cda4d7cf3
joaopedrolop <joao.p.freire6@aluno.senai.br>
2025-03-14 13:28:28 -0300

    pagina

--- Exercicio/app.js
+++ Exercicio/app.js
@@ -4,9 +4,20 @@ const PORT = 3000;
 
 const app = express();
 
+const index = "<a href='/sobre'>Sobre</a><a href='/info'</a>";
+
 app.get('/', (req, res) => {
-    res.send("olá senai")
+    res.send("olá senai");
 });
+
+app.get("/sobre", (req, res) => {
+    res.send("vc esta na pagina sobre");
+});
+
+app.get("/info", (req, res) => {
+    res.send("vc esta na pagina info");
+});
+
 
 app.listen(PORT, () => {
     console.log(`servidor sendo execultado na portal 3000 ${PORT}!`);
